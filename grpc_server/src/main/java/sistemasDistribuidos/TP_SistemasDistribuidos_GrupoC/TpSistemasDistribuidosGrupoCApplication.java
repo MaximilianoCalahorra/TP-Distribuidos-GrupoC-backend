9765674410b6071ea9517dc844ca002a03b516b9
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2025-08-31 19:30:12 -0300

    JMN | Subimos proyecto base para grpc_server

---
+++ grpc_server/src/main/java/sistemasDistribuidos/TP_SistemasDistribuidos_GrupoC/TpSistemasDistribuidosGrupoCApplication.java
@@ -0,0 +1,13 @@
+package sistemasDistribuidos.TP_SistemasDistribuidos_GrupoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSistemasDistribuidosGrupoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSistemasDistribuidosGrupoCApplication.class, args);
+	}
+
+}
---
+++ grpc_server/src/test/java/sistemasDistribuidos/TP_SistemasDistribuidos_GrupoC/TpSistemasDistribuidosGrupoCApplicationTests.java
@@ -0,0 +1,13 @@
+package sistemasDistribuidos.TP_SistemasDistribuidos_GrupoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSistemasDistribuidosGrupoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
