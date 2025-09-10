5af8fca1edd0be2a070d787557c6817298757f65
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-09-10 14:12:28 -0300

    MC | Modificaciones para incluir el id

--- grpc_server/src/main/java/sistemasDistribuidos/TP_SistemasDistribuidos_GrupoC/DTOs/InventarioDTO.java
+++ grpc_server/src/main/java/sistemasDistribuidos/TP_SistemasDistribuidos_GrupoC/DTOs/InventarioDTO.java
@@ -9,13 +9,15 @@ import sistemasDistribuidos.TP_SistemasDistribuidos_GrupoC.Enums.Categoria;
 @Setter
 @NoArgsConstructor
 public class InventarioDTO {
+	private Long idInventario;
     private Categoria categoria;
     private String descripcion;
     private int cantidad;
     private boolean eliminado;
 
     // Constructor con todos los campos
-    public InventarioDTO(Categoria categoria, String descripcion, int cantidad, boolean eliminado) {
+    public InventarioDTO(Long idInventario, Categoria categoria, String descripcion, int cantidad, boolean eliminado) {
+    	this.idInventario = idInventario;
         this.categoria = categoria;
         this.descripcion = descripcion;
         this.cantidad = cantidad;
