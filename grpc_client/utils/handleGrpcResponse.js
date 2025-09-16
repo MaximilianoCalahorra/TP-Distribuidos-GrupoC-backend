2a59be3249320c59b3a64537f2c01d79c826d052
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-09-15 23:10:06 -0300

    MC | Nueva corrección relacionada a la anterior

--- grpc_client/utils/handleGrpcResponse.js
+++ grpc_client/utils/handleGrpcResponse.js
@@ -4,7 +4,7 @@ import { mapGrpcErrorToHttp } from "./mapGrpcErrorAHttp.js";
 export const handleGrpcResponse = (res, error, response) => {
   //Si hay algún error:
   if (error) {
-    const { status, message } = mapGrpcErrorAHttp(error); //Obtenemos estado y mensaje.
+    const { status, message } = mapGrpcErrorToHttp(error); //Obtenemos estado y mensaje.
     return res.status(status).json({ error: message }); //Retornamos el estado y el mensaje.
   }
   return res.json(response); //Si no hay errores, retornamos la respuesta del servidor gRPC.
