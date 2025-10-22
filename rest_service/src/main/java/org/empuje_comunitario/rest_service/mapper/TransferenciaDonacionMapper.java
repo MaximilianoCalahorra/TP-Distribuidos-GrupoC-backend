51cd354d90c579b14ce5d85f121202f836a24dda
Federico Noceti <nfedericotomas@gmail.com>
2025-10-22 14:42:16 -0300

    Creacion de la base de Rest

---
+++ rest_service/src/main/java/org/empuje_comunitario/rest_service/mapper/TransferenciaDonacionMapper.java
@@ -0,0 +1,53 @@
+package org.empuje_comunitario.rest_service.mapper;
+
+import java.util.stream.Collectors;
+
+import org.empuje_comunitario.rest_service.dto.TransferenciaDonacionDTO;
+import org.empuje_comunitario.rest_service.model.TransferenciaDonacion;
+
+public class TransferenciaDonacionMapper {
+	// =======================
+    // Entidad <-> DTOs
+    // =======================
+	
+	// TransferenciaDonacionDTO <-> TransferenciaDonacion
+    public static TransferenciaDonacionDTO aDTO(TransferenciaDonacion entidad) {
+        if (entidad == null) return null;
+
+        TransferenciaDonacionDTO dto = new TransferenciaDonacionDTO();
+        
+        dto.setIdTransferenciaDonacion(entidad.getIdTransferenciaDonacion());
+        dto.setIdSolicitudDonacion(entidad.getIdSolicitudDonacion());
+        dto.setIdOrganizacionDonante(entidad.getIdOrganizacionDonante());
+        dto.setIdOrganizacionReceptora(entidad.getIdOrganizacionReceptora());
+        dto.setFechaHora(entidad.getFechaHora());
+        dto.setItems(
+                entidad.getItems().stream()
+                        .map(ItemDonacionMapper::aDTO)
+                        .collect(Collectors.toList()));
+        
+        return dto;
+    }
+
+    public static TransferenciaDonacion aEntidad(TransferenciaDonacionDTO dto) {
+        if (dto == null) return null;
+
+        TransferenciaDonacion entidad = new TransferenciaDonacion();
+        
+        if (dto.getIdTransferenciaDonacion() != null && dto.getIdTransferenciaDonacion() > 0) {
+        	entidad.setIdTransferenciaDonacion(dto.getIdTransferenciaDonacion());
+        }
+        
+        entidad.setIdSolicitudDonacion(dto.getIdSolicitudDonacion());
+        entidad.setIdSolicitudDonacion(dto.getIdSolicitudDonacion());
+        entidad.setIdOrganizacionDonante(dto.getIdOrganizacionDonante());
+        entidad.setIdOrganizacionReceptora(dto.getIdOrganizacionReceptora());
+        entidad.setFechaHora(dto.getFechaHora());
+        entidad.setItems(
+                dto.getItems().stream()
+                        .map(ItemDonacionMapper::aEntidad)
+                        .collect(Collectors.toList()));
+
+        return entidad;
+    }
+}
