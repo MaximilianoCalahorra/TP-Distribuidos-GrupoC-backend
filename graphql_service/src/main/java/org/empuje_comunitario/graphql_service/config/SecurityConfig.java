c7399cb232f02e6209b9a250b764781ca49cfc6a
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-10-23 11:33:36 -0300

    MC | Configuración de CORS

--- graphql_service/src/main/java/org/empuje_comunitario/graphql_service/config/SecurityConfig.java
+++ graphql_service/src/main/java/org/empuje_comunitario/graphql_service/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package org.empuje_comunitario.graphql_service.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.empuje_comunitario.graphql_service.service.implementations.UserSecurityService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -12,6 +16,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -47,5 +54,16 @@ public class SecurityConfig {
 	    authProvider.setPasswordEncoder(passwordEncoder());
 	    return authProvider;
 	}
+	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource(@Value("${cors.allowed.origin}") String allowedOrigin) {
+        CorsConfiguration cc = new CorsConfiguration();
+        cc.setAllowedOrigins(Arrays.asList(allowedOrigin));
+        cc.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
+        cc.setAllowedHeaders(List.of("Authorization", "Content-Type", "email", "password", "roles"));
+        cc.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cc);
+        return source;
+    }
 }
-
